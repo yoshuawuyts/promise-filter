67b0b87f3b63daae069fa659cd1bd8cfd80e5fd7
Yoshua Wuyts <i@yoshuawuyts.com>
2015-01-24 04:28:33 +0100

    .

---
+++ test.js
@@ -0,0 +1,28 @@
+const test    = require('tape')
+const filter  = require('./')
+const Promise = require('native-or-bluebird')
+
+test('promise-filter should assert input types', function(t) {
+  t.plan(1)
+  t.throws(filter.bind(null, 123))
+})
+
+test('promise-filter should filter a fn', function(t) {
+  t.plan(1)
+  const res = Promise.resolve([1, 2, 3])
+    .then(filter(filterFn))
+    .then(checkFn)
+    .catch(handleErr)
+
+  function filterFn(val) {
+    return val >= 2
+  }
+
+  function checkFn(val) {
+    t.deepEqual([2, 3], val)
+  }
+
+  function handleErr() {
+    t.fail('catch')
+  }
+})
